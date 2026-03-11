45379fa8b5626a378bf2346143f51b6c8d0d2e41
jp <jpespechit@gmail.com>
2026-03-11 17:09:06 -0300

    Solving Module 04 - Primitive Types

--- exercises/04_primitive_types/primitive_types3.rs
+++ exercises/04_primitive_types/primitive_types3.rs
@@ -1,6 +1,6 @@
 fn main() {
     // TODO: Create an array called `a` with at least 100 elements in it.
-    // let a = ???
+    let a = [0; 100];
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
@@ -9,3 +9,5 @@ fn main() {
         panic!("Array not big enough, more elements needed");
     }
 }
+
+
